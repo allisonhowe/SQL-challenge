fc7bcb8c7d59a415159985d7e3286873bbd4106f
Allison Howe <allisonhowe77@gmail.com>
2020-08-13 18:35:56 -0400

    update3

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -7,7 +7,9 @@ employees.emp_no = salaries.emp_no;
 
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
-select first_name, last_name, hire_date from employees where extract(year from hire_date) = 1986;
+select first_name, last_name, hire_date 
+from employees 
+where extract(year from hire_date) = 1986;
 
 
 -- List the manager of each department with the following information: 
@@ -31,10 +33,33 @@ on dept_emp.dept_no = departments.dept_no;
 
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex 
+from employees 
+where first_name = 'Hercules' and last_name like 'B%';
+
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+inner join dept_emp
+on employees.emp_no = dept_emp.emp_no
+inner join departments
+on dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales';
+
 
 -- List all employees in the Sales and Development departments, 
 -- including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+inner join dept_emp
+on employees.emp_no = dept_emp.emp_no
+inner join departments
+on dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
+
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count(*) from employees
+group by last_name
+order by count(*) desc
